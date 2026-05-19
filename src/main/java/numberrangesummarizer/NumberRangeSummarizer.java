2f51832d3f0aea233fc99ff480e9aa07287de98e
Nadine MacDonald <NadineMacDonald222nadinemacdonald222@gmail.com>
2026-05-19 16:00:35 +0200

    Reset main branch to clean baseline

--- src/main/java/numberrangesummarizer/NumberRangeSummarizer.java
+++
@@ -1,8 +0,0 @@
-package numberrangesummarizer;
-
-import java.util.Collection;
-
-public interface NumberRangeSummarizer {
-    Collection<Integer> collect(String input);
-    String summarizeCollection(Collection<Integer> input);
-}
